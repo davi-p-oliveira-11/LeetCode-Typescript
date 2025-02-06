2dea877b860ddda9619f1eb6c0b297db060a45fe
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-02-06 15:53:22 -0300

    first commit

---
+++ solutions/toLowerCase.ts
@@ -0,0 +1,35 @@
+// Source : https://leetcode.com/problems/to-lower-case/description/
+// Author : Davi P. Oliveira / ********
+// Date : 06/02/2025
+
+/* 
+ 709. To Lower Case
+Status : Solved
+Dificulty : Easy
+
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+Example 1:
+
+Input: s = "Hello"
+Output: "hello"
+Example 2:
+
+Input: s = "here"
+Output: "here"
+Example 3:
+
+Input: s = "LOVELY"
+Output: "lovely"
+ 
+Constraints:
+
+1 <= s.length <= 100
+s consists of printable ASCII characters.
+*/
+
+// Solution
+
+function toLowerCase(s: string): string {
+  return s.toLowerCase();
+}
